7c5bd29200eedf4a9e4a7a3b1ace589638e8a89a
Damjan <Damjan@DESKTOP-33QRDOG>
2023-08-10 17:18:35 +0200

    Add project files.

---
+++ HomeworkApi/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeworkApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> GetAllUsernames()
+        {
+            return Ok(StaticDb.Usernames);
+        }
+
+        [HttpGet(("{index}"))]
+        public ActionResult<List<string>> GetUsernameByIndex(int index)
+        {
+            try
+            {
+                if (index < 0)
+                {
+                    return BadRequest();
+                }
+
+                if (index >= StaticDb.Usernames.Count)
+                {
+                    return NotFound($"There is no username with an index:{index} \n Try another one.");
+                }
+                
+
+                return Ok(StaticDb.Usernames[index]);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+        }
+    }
+}
---
+++ HomeworkApi/StaticDb.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeworkApi
+{
+    public class StaticDb
+    {
+        public static List<string> Usernames = new List<string>()
+        {
+            "damjan123",
+            "seavus123",
+            "webapi101"
+        };
+    }
+}
